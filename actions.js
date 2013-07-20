38342127a2a42212375e82665c1783ad82f27452
Jorge Lira <coque_16@hotmail.com>
2013-07-20 17:13:21 -0500

    asdf

--- actions.js
+++ actions.js
@@ -10,12 +10,11 @@ $(document).ready(function(){
             alert('code: '    + error.code    + '\n' +
                   'message: ' + error.message + '\n');
         }
-
-        navigator.geolocation.getCurrentPosition(onSuccess, onError);
-    },false);
+navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    }, false);
 });
 
-function initialize(lat, lon) {
+function initialize(lat,lon){
     //Posición del mapa
     var latlng = new google.maps.LatLng(lat, lon);
     var myOptions = {
